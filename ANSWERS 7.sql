90bfb58e9d68c48421ca031f2da570b01d08429a
Sylvester <novietech160@gmail.com>
2025-05-01 00:45:05 +0300

    Add files via upload

---
+++ ANSWERS 7.sql
@@ -0,0 +1,18 @@
+USE Names;
+-- CREATE ORDERSITEMS TABLE
+CREATE TABLE OrderItems (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO OrderItems (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
